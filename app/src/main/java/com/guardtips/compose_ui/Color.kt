05b211d03e9a7862d53d5613deb12a0c1406fd66
Ji Seok <ygs01033@naver.com>
2023-11-03 10:49:35 +0900

    feat: 웹뷰 로딩시 CircularProgressIndicator 추가

--- app/src/main/java/com/guardtips/compose_ui/Color.kt
+++ app/src/main/java/com/guardtips/compose_ui/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val PrimaryTextColor = Color(0xFF000000)
 val White = Color(0xFFFFFFFF)
 val SecondaryVariantColor = Color(0xFF717175)
-
+val MainBlueColor = Color(0xFF056FE7)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
